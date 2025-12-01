7e169e3ea9369ad7e8a4d1a3a083b60dcd3c419d
Khusha3560 <khushboo.sharma@ekarigar.com>
2025-12-01 16:49:50 +0530

    worked on mohit sir messages update in pages

--- src/app/components/chat-options-popover/chat-options-popover.component.ts
+++ src/app/components/chat-options-popover/chat-options-popover.component.ts
@@ -18,11 +18,32 @@ export class ChatOptionsPopoverComponent {
     this.popoverCtrl.dismiss({ selected: option });
   }
 
+  // get menuOptions() {
+  //   if (this.chatType === 'group') {
+  //     return ['Group Info','Group Media','Search','Add Members', 'Exit Group', 'clear chat'];
+  //   } else {
+  //     return ['View Contact', 'Search', 'Mute Notifications wip', 'clear chat'];
+  //   }
+  // }
   get menuOptions() {
-    if (this.chatType === 'group') {
-      return ['Group Info','Group Media','Search','Add Members', 'Exit Group', 'clear chat'];
-    } else {
-      return ['View Contact', 'Search', 'Mute Notifications wip', 'clear chat'];
-    }
+  if (this.chatType === 'group') {
+    return [
+      'Group Info (DemmRoom Info)',
+      'Group Media (DemmVault)',
+      'Search (FindTell)',
+      'Add Members (Add Demmians)',
+      'Exit Group (Leave DemmRoom)',
+      'Clear Chat (Clear DemmChat)'
+    ];
+  } else {
+    return [
+      'View Contact (View Demmian)',
+      'Search (FindTell)',
+      'Mute Notifications (QuietTell) wip',
+      'Clear Chat (Clear DemmChat)'
+    ];
   }
+}
+
+
 }
